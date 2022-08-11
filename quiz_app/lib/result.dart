e484cbe33256d9bb21bf9adc1954d0a7b2f2b378
acatarinaoaraujo <acoa_@hotmail.com>
2022-08-10 20:40:37 -1000

    final changes

--- quiz_app/lib/result.dart
+++ quiz_app/lib/result.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
@@ -6,18 +8,23 @@ class Result extends StatelessWidget {
 
   Result(this.resultScore, this.resetHandler);
 
-  String get resultPhrase {
+  List<String> get resultPhrase {
     String resultText;
+    int isFeedbackPositive;
     if (resultScore <= 70) {
       resultText = 'You need to practice more!';
+      isFeedbackPositive = 0;
     } else if (resultScore <= 80) {
       resultText = 'You did well!';
+      isFeedbackPositive = 1;
     } else if (resultScore <= 90) {
       resultText = 'You did very well!';
+      isFeedbackPositive = 1;
     } else {
       resultText = 'Congratulations, you did great!';
+      isFeedbackPositive = 1;
     }
-    return resultText;
+    return [resultText, isFeedbackPositive.toString()];
   }
 
   @override
@@ -25,27 +32,63 @@ class Result extends StatelessWidget {
     return Center(
       child: Container(
         width: double.infinity,
-        height: 120.0,
+        height: 200.0,
         margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: int.parse(resultPhrase[1]) == 0
+              ? Color.fromARGB(255, 160, 73, 67)
+              : Color.fromARGB(255, 70, 166, 73),
+          border: Border.all(
+            color: Color.fromARGB(156, 117, 149, 226),
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
         child: Column(
           children: <Widget>[
+            SizedBox(height: 10),
             Text(
-              resultPhrase,
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              resultPhrase[0],
+              style: TextStyle(
+                  fontSize: 28,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
               textAlign: TextAlign.center,
             ),
-            FlatButton(
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your final score: " + resultScore.toString() + " ",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                Icon(
+                  int.parse(resultPhrase[1]) == 0
+                      ? Icons.thumb_down
+                      : Icons.thumb_up,
+                  color: int.parse(resultPhrase[1]) == 0
+                      ? Color.fromARGB(255, 237, 72, 60)
+                      : Color.fromARGB(255, 75, 244, 81),
+                  size: 30.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
               child: Text(
                 'Restart Quiz!',
               ),
-              textColor: Colors.white,
+              textColor: Colors.blue,
+              color: Colors.white,
               onPressed: resetHandler,
-            ),
-            Icon(
-              Icons.diamond_sharp,
-              color: Colors.amber,
-              size: 30.0,
-              semanticLabel: 'Text to announce in accessibility modes',
             ),
           ],
         ),
